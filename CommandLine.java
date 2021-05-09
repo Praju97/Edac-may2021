3e9ae8b277ee0ca2d64c3affc362a995710e329d
Praju97 <33142419+Praju97@users.noreply.github.com>
2021-05-09 12:00:51 +0530

    Add files via upload

---
+++ CommandLine.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class CommandLine
+{
+	public static void main(String args[])
+	{
+		int x=Integer.parseInt(args[0]);
+		int y=Integer.parseInt(args[1]);
+		
+		int sum=x+y;
+		
+		System.out.println("Sum is: "+sum);
+		
+	}
+}
